3adac67d59492a329b940eba3a89844c5c2e2b9b
zorazzhang <zorazzhang@tencent.com>
2023-07-19 15:08:01 +0800

    主干修改代码

--- src/App.js
+++ src/App.js
@@ -8,18 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div>asdfasdf</div>
         <p>
-          sadfasdfa
           Edit <code>src/App.j111s</code> and save to reload1111.
         </p>
         <div>xinzengwenan111</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
