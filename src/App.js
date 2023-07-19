20967fd019c4549ef889a2794fe755d66ddb01db
zorazzhang <zorazzhang@tencent.com>
2023-07-19 14:45:43 +0800

    主干提交文案

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ function App() {
         >
           Learn React
         </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
       </header>
     </div>
   );
