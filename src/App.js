68d999773e287585a62ef4972e8573f971318eb1
zorazzhang <zorazzhang@tencent.com>
2023-07-19 15:03:48 +0800

    修改样式

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>asdfasdf</div>
+        <div>HELLO</div>
         <p>
           sadfasdfa
           Edit <code>src/App.j111s</code> and save to reload1111.
